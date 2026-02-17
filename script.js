1a8cc9b338314050a15ebe226c8099bd15249467
yasin-wajihaa <wajihaazafar@gmail.com>
2026-02-17 19:31:20 +0530

    added the theme togglel (light dark)

--- script.js
+++ script.js
@@ -68,3 +68,19 @@ function renderEvents() {
 }
 
 renderEvents();
+
+const btn = document.getElementById("theme-toggle");
+
+if (localStorage.getItem("theme") === "dark") {
+    document.body.classList.add("dark");
+}
+
+btn.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+
+    const isDark = document.body.classList.contains("dark");
+    localStorage.setItem("theme", isDark ? "dark" : "light");
+});
+
+
+
